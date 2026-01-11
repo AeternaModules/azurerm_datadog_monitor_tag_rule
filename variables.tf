5ba97ba55a2d74533d23ddbc15d7fb548e3f20ee
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:44:45 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "datadog_monitor_tag_rules" {
+  description = <<EOT
+Map of datadog_monitor_tag_rules, attributes below
+Required:
+    - datadog_monitor_id
+Optional:
+    - name
+    - log (block):
+        - aad_log_enabled (optional)
+        - filter (optional, block):
+            - action (required)
+            - name (required)
+            - value (required)
+        - resource_log_enabled (optional)
+        - subscription_log_enabled (optional)
+    - metric (block):
+        - filter (optional, block):
+            - action (required)
+            - name (required)
+            - value (required)
+EOT
+
+  type = map(object({
+    datadog_monitor_id = string
+    name               = optional(string, "default")
+    log = optional(object({
+      aad_log_enabled = optional(bool)
+      filter = optional(object({
+        action = string
+        name   = string
+        value  = string
+      }))
+      resource_log_enabled     = optional(bool)
+      subscription_log_enabled = optional(bool)
+    }))
+    metric = optional(object({
+      filter = optional(object({
+        action = string
+        name   = string
+        value  = string
+      }))
+    }))
+  }))
+}
+
