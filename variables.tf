eff580e44fa4f950bdba156cbc8b17189df07548
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:50:22 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ EOT
 
   type = map(object({
     datadog_monitor_id = string
-    name               = optional(string, "default")
+    name               = optional(string) # Default: "default"
     log = optional(object({
       aad_log_enabled = optional(bool)
       filter = optional(object({
