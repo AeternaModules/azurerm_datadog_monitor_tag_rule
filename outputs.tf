d179bdecd045eaa27ba55667af107e96b4241f23
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "datadog_monitor_tag_rules" {
-  description = "All datadog_monitor_tag_rule resources"
-  value       = azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules
-}
 output "datadog_monitor_tag_rules_datadog_monitor_id" {
-  description = "List of datadog_monitor_id values across all datadog_monitor_tag_rules"
-  value       = [for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : v.datadog_monitor_id]
+  description = "Map of datadog_monitor_id values across all datadog_monitor_tag_rules, keyed the same as var.datadog_monitor_tag_rules"
+  value       = { for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : k => v.datadog_monitor_id }
 }
 output "datadog_monitor_tag_rules_log" {
-  description = "List of log values across all datadog_monitor_tag_rules"
-  value       = [for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : v.log]
+  description = "Map of log values across all datadog_monitor_tag_rules, keyed the same as var.datadog_monitor_tag_rules"
+  value       = { for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : k => v.log }
 }
 output "datadog_monitor_tag_rules_metric" {
-  description = "List of metric values across all datadog_monitor_tag_rules"
-  value       = [for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : v.metric]
+  description = "Map of metric values across all datadog_monitor_tag_rules, keyed the same as var.datadog_monitor_tag_rules"
+  value       = { for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : k => v.metric }
 }
 output "datadog_monitor_tag_rules_name" {
-  description = "List of name values across all datadog_monitor_tag_rules"
-  value       = [for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : v.name]
+  description = "Map of name values across all datadog_monitor_tag_rules, keyed the same as var.datadog_monitor_tag_rules"
+  value       = { for k, v in azurerm_datadog_monitor_tag_rule.datadog_monitor_tag_rules : k => v.name }
 }
 
